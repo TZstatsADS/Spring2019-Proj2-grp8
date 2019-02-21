d852c2d7b62ded044e961e8528d84f42a0234593
cqx5037 <cqx5037@psu.edu>
2019-02-20 21:21:02 -0500

    Modified 1

--- app/server.R
+++ app/server.R
@@ -1,168 +1,249 @@
-library(shiny)
-library(choroplethr)
-library(choroplethrZip)
-library(dplyr)
-library(leaflet)
-library(maps)
-library(rgdal)
+library(geosphere)
+library(ggmap)
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", 
-           "Chelsea and Clinton",
-           "East Harlem", 
-           "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", 
-           "Lower Manhattan",
-           "Lower East Side", 
-           "Upper East Side", 
-           "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
-
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+museum<-na.omit(read.csv("museums.csv"))
+theatre<-read.csv('theatre.csv')
+restaurant<- read.csv('restaurant_new.csv')
+type <- unique(as.character(restaurant$TYPE))
+namedata<-c("Deli","Museum","Theater","Gallery","Library","Market")
+Deli<-na.omit(read.csv("Deli.csv",header=T))
+Deli$URL<-"Unavailable"
+Market<-na.omit(read.csv("Market.csv",header=T))
+Market$URL<-"Unavailable"
+Gallery<-na.omit(read.csv("Gallery.csv",header=T))
+Library<-na.omit(read.csv("Library.csv",header=T))
+Museum<-na.omit(read.csv("Museum.csv",header=T))
+Restaurant<-na.omit(read.csv("Restaurant.csv",header=T))
+Restaurant$URL<-"Unavailable"
+Theater<-na.omit(read.csv("Theater.csv",header=T))
+all_data<-list(Deli=Deli,Market=Market,Gallery=Gallery,Library=Library,Museum=Museum,Restaurant=Restaurant,Theater=Theater)
+register_google("AIzaSyA8OuCvy04PC3N-K9y6DdEc32hUpNyUrl8")
+load("output/sub.station.RData")
+load("output/bus.stop.RData")
+# Define a server for the Shiny app
+function(input, output) {
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
+  
+  #################Clear Choices############
+  observeEvent(input$button2,{
+    proxy<-leafletProxy("map1")
+    proxy %>%
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) 
+    # %>%
+    #   removeMarker(layerId="1") %>%
+    #   addMarkers(data=housing,
+    #              lng=~lng,
+    #              lat=~lat,
+    #              clusterOptions=markerClusterOptions(),
+    #              group="housing_cluster")
+    
   })
-  
-  ## Panel 1: summary plots of time trends, 
-  ##          unit price and full price of sales. 
-  
-  output$distPlot <- renderPlot({
-    
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
-  })
-  
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
-  })
-  
-  ## Panel 3: leaflet
-  output$map <- renderLeaflet({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    # From https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data
-    NYCzipcodes <- readOGR("../data/ZIP_CODE_040114.shp",
-                           #layer = "ZIP_CODE", 
-                           verbose = FALSE)
-    
-    selZip <- subset(NYCzipcodes, NYCzipcodes$ZIPCODE %in% count.df.sel$region)
-    
-    # ----- Transform to EPSG 4326 - WGS84 (required)
-    subdat<-spTransform(selZip, CRS("+init=epsg:4326"))
-    
-    # ----- save the data slot
-    subdat_data=subdat@data[,c("ZIPCODE", "POPULATION")]
-    subdat.rownames=rownames(subdat_data)
-    subdat_data=
-      subdat_data%>%left_join(count.df, by=c("ZIPCODE" = "region"))
-    rownames(subdat_data)=subdat.rownames
-    
-    # ----- to write to geojson we need a SpatialPolygonsDataFrame
-    subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
-    
-    # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
-    # Create a continuous palette function
-    pal <- colorNumeric(
-      palette = "Blues",
-      domain = subdat$POPULATION
-    )
-    
-    leaflet(subdat) %>%
-      addTiles()%>%
-      addPolygons(
-        stroke = T, weight=1,
-        fillOpacity = 0.6,
-        color = ~pal(POPULATION)
+  ###map
+  output$map1 <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles('Esri.WorldTopoMap') %>%
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) %>%
+      addMarkers(data=restaurant,
+                 lng=restaurant$LON,
+                 lat=restaurant$LAT,
+                 clusterOptions=markerClusterOptions(),
+                 group="housing_cluster"
       )
   })
-})
+  
+  # observe({
+  #   
+  #   housing_sort=marksInBounds()
+  #   
+  #   if(nrow(housing_sort)!=0){
+  #     
+  #     action=apply(housing_sort,1,function(r){
+  #       addr=r["addr"]
+  #       lat=r["lat"]
+  #       lng=r["lng"]
+  #       paste0("<a class='go-map' href='' data-lat='",lat,"'data-lng='",lng,"'>",addr,'</a>')   
+  #     }
+  #     )
+  #     
+  #     housing_sort$addr=action
+  #     output$rank <- renderDataTable(housing_sort[,c("addr","price","bedrooms","bathrooms")],escape=FALSE)
+  #     
+  #     
+  #     
+  #     
+  #   }
+  #   else{
+  #     
+  #     output$rank=renderDataTable(housing_sort[,c("addr","price","bedrooms","bathrooms")])
+  #   }
+  # })
+  # 
+  
+  # Fill in the spot we created for a plot
+  output$table1 <- renderDataTable({
+
+    if (input$region1 == 'Museums'){
+      print(museum[,1:3])
+
+    }
+    else if(input$region1 == 'Theatre'){
+      print(theatre)
+
+    }
+    else if(input$region1 == 'Restaurant') {
+      if (input$type1 == 'ALL'){
+        print(restaurant)
+      }
+      else{
+        print(restaurant[restaurant$TYPE == as.character(input$type1),])
+      }
+
+    }
+    })
+   output$table2 <- renderDataTable({
+    
+    if (input$region2 == 'Museums'){
+      print(museum[,1:3])
+      
+    }
+    else if(input$region2 == 'Theatre'){
+      print(theatre)
+      
+    }
+    else if(input$region2 == 'Restaurant') {
+      if (input$type1 == 'ALL'){
+        print(restaurant)
+      }
+      else{
+        print(restaurant[restaurant$TYPE == as.character(input$type1),])
+      }      
+    }
+  })
+    output$table3 <- renderDataTable({
+      
+      if (input$region3 == 'Museums'){
+        print(museum[,1:3])
+        
+      }
+      else if(input$region3 == 'Theatre'){
+        print(theatre)
+        
+      }
+      else if(input$region3 == 'Restaurant') {
+        if (input$type1 == 'ALL'){
+          print(restaurant)
+        }
+        else{
+          print(restaurant[restaurant$TYPE == as.character(input$type1),])
+        }        
+      }
+    })
+    
+    observeEvent(input$submit,{
+      if(input$location=="Current Location"){
+        coord<-getlatlng()
+        lat<-coord[1]
+        long<-coord[2]
+        output$map<-renderLeaflet(
+          {    map <- leaflet() %>% addTiles()
+          
+          map <- addControlGPS(map, options = gpsOptions(position = "topleft", activate = TRUE, 
+                                                         autoCenter = TRUE, maxZoom = 10, 
+                                                         setView = TRUE))
+          activateGPS(map)
+          
+          
+          })
+        output$action=renderPrint({coord})
+        
+      }else{
+        coord<-geocode(input$location)
+        output$action=renderPrint({coord})
+        lat<-as.numeric(coord[2])
+        long<-as.numeric(coord[1])
+        if(is.na(lat)&is.na(long)){ 
+          output$errorput<-renderText("Please enter a valid address")
+        }else{
+          output$map<-renderLeaflet(
+            {    map<-leaflet() %>%  addTiles()%>%
+              addMarkers(lng=long,lat=lat)
+            
+            
+            })
+        }
+      }
+      
+      #roll the dice for random choice
+      
+      
+      output$locationin<-renderText(input$location)
+      observeEvent(input$submit4,{
+        index<-sample(1:4,2,replace=F)
+        choice<-c("Theater","Museum","Gallery","Library","Restaurant")
+        choice1<-choice[index[1]]
+        choice2<-choice[index[2]]
+        index[3]<-length(choice)
+        choice3<-choice[index[3]]
+        output$c1<- renderText({choice1})
+        output$c2<- renderText({choice2})
+        
+        data_candidate<-all_data[c(choice1,choice2,choice3)]
+        
+        ##Get candidates according to the radius:
+        data_select<-lapply(data_candidate,get_candidate,Lon0=long,Lat0=lat,r=input$distance*1000)
+        
+        targetplan<-lapply(data_select,randomchoice)
+        output$target1<-renderText(as.character(targetplan[[1]][1,"NAME"]))
+        output$target2<-renderText(as.character(targetplan[[2]][1,"NAME"]))
+        output$target3<-renderText(as.character(targetplan[[3]][1,"NAME"]))
+        
+        leafletProxy("map")%>%
+          clearMarkerClusters()
+        
+        for (i in 1:3){
+          leafletProxy("map",data=targetplan[[i]]) %>%
+            addMarkers(clusterOptions = markerClusterOptions(),
+                       lng=targetplan[[i]]$LON,lat=targetplan[[i]]$LAT,
+                       icon=list(iconUrl=paste('icon/',choice[index[i]],'.png',sep = ""),iconSize=c(20,20)),
+                       popup=paste("Name:",targetplan[[i]]$NAME,"<br/>",
+                                   "Tel:",targetplan[[i]]$TEL,"<br/>",
+                                   "Website:",a(targetplan[[i]]$URL, href = targetplan[[i]]$URL),"<br/>", # warning appears
+                                   "Address:",targetplan[[i]]$ADDRESS),
+                       group=choice[index[i]],layerId = i )
+        }
+        
+        observeEvent(input$Subway,{
+          p<-input$Subway
+          proxy<-leafletProxy("map")
+          
+          if(p==TRUE){
+            proxy %>% 
+              addMarkers(data=sub.station, ~lng, ~lat,label = ~info,icon=icons(
+                iconUrl = "output/icons8-Bus-48.png",
+                iconWidth = 10, iconHeight = 10),group="subway")
+          }
+          else proxy%>%clearGroup(group="subway")
+          
+        })
+        
+        ###############bus###############
+        observeEvent(input$Bus,{
+          p<-input$Bus
+          proxy<-leafletProxy("map")
+          
+          if(p==TRUE){
+            proxy %>% 
+              addMarkers(data=bus.stop, ~lng, ~lat,label = ~info,icon=icons(
+                iconUrl = "output/icons8-Bus-48.png",
+                iconWidth =10, iconHeight = 10),layerId=as.character(bus.stop$info))
+          }
+          else proxy%>%removeMarker(layerId=as.character(bus.stop$info))
+          
+        })
+        
+        
+      })
+      
+    }) 
+    
+  }
--- app/ui.r
+++ app/ui.r
@@ -1,42 +1,183 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
 library(shiny)
+library(shinydashboard)
 library(leaflet)
+library(ggplot2)
+library(shinyjs)
+library(shinydashboard)
+library(shiny)
+library(shinyBS)
+library(shinyWidgets)
+library(leaflet.extras)
+library(rebird)
+library(geosphere)
+library(ggmap)
+# Use a fluid Bootstrap layout
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
-  
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1"))),
-      leafletOutput("map", width = "80%", height = "400px")
-    )
- )
-))
+restaurant<- read.csv('restaurant_new.csv')
+type <- unique(as.character(restaurant$TYPE))
 
+ui <- dashboardPage(skin = "yellow",
+                    dashboardHeader(title= "Travel Suggestion"),
+                    dashboardSidebar(
+                      sidebarMenu(
+                        id="tabs",
+                        menuItem("Page1",tabName = "prerequisite1",icon = icon("book")),
+                        menuItem("Page2", tabName = "overview",icon = icon("dashboard")),
+                        menuItem("Page3", tabName = "challenges",icon = icon("cogs"))
+                      )
+                    ),
+                    dashboardBody(
+                      tags$head(
+                        tags$link(rel = "stylesheet", type = "text/css", href = "stylecssMouseExp.css")
+                        
+                        # tags$style(HTML(
+                        # '.popover-title{
+                        # color:black;
+                        # font-size:16px;
+                        # background-color: orange
+                        # }'
+                        # ))
+                      ),
+                      tabItems(
+                        
+                        #prerequiste tab content
+                        
+                        tabItem(tabName = "prerequisite1",
+                                
+                              
+                           
+                                
+                                
+                                div(style = "text-align:center",
+                                    bsButton("nextbutton", "Explore", icon("wpexplorer"), size = "large",style = "warning"))
+                                
+                        ),
+                        
+                        # First tab content
+                        tabItem(tabName = "overview",
+                                #tags$a(href='http://stat.psu.edu/',   tags$img(src='logo.png', align = "left", width = 180)),
+
+                                # div(style = "text-align:center",
+                                #     bsButton("start", "GO", icon("bolt"), size = "large",style = "warning")),
+                                fluidPage(    
+                                  
+                                  # Give the page a title
+                                  
+                                  # Generate a row with a sidebar
+                                  sidebarLayout(      
+                                    
+                                    # Define the sidebar with one input
+                                    sidebarPanel(
+                                      selectInput("region1", "Choice 1:", 
+                                                  choices=c('Restaurant','NA','Museums','Theatre')),
+                                      selectInput("region2", "Choice 2:", 
+                                                  choices=c('NA','Museums','Theatre','Restaurant')),
+                                      selectInput("region3", "Choice 3:", 
+                                                  choices=c('NA','Museums','Theatre','Restaurant')),
+                                      conditionalPanel("input.region1 == 'Restaurant' || input.region2 == 'Restaurant' || input.region3 == 'Restaurant'" ,
+                                                       selectInput('type1','Restaurant Type',c("ALL",type))),
+                                      
+                                      hr(),
+                                      column(9, 
+                                             br(),
+                                             br(),
+                                             tabsetPanel(type = "tabs",
+                                                         tabPanel("Choice 1", dataTableOutput("table1")),
+                                                         tabPanel("Choice 2", dataTableOutput("table2")),
+                                                         tabPanel("Choice 3", dataTableOutput("table3"))
+                                             )
+                                             
+                                      )
+                                    ),
+                                    
+                                    
+                                    
+                                    
+                                    # Create a spot for the barplot
+                                    mainPanel(
+                                      column(width=1, 
+                                             actionButton("button2",label="Reset Map" 
+                                                          #,style="padding:12px; font-size:80%;color: #fff; background-color: #337ab7; border-color: #2e6da4"
+                                             )),
+                                      
+                                      leafletOutput("map1", width = "200%", height = 400)
+                                    )
+                                  )
+                                  
+                                )
+                                
+                             
+                                
+                                
+                        ),
+                        
+                        # Second tab content
+                        tabItem(tabName = "challenges",
+                                titlePanel("Plan your day"),
+                                
+                                # Sidebar with a slider input for number of bins 
+                                sidebarLayout(
+                                  sidebarPanel(id="control1",fixed = T,draggable = T,class = "panel panel-default",top = 80,
+                                               
+                                               left = 55, right = "auto", bottom = "auto",
+                                               
+                                               width = 280, height = 550,
+                                               
+                                               textInput("location",label = h3("Enter Your Location"),"Current Location"),
+                                               actionButton("submit","Mark Your Location",icon("map-marker")),
+                                               sliderInput("distance", "Distance From You (in km)", min = 0, max = 20, value = 1, step= 0.1),
+                                               actionButton("submit4", label="Go!",style="opacity:0.8",align="left")
+                                           
+                                  ),
+                                  
+                                  # Show a plot of the generated distribution
+                                  mainPanel(
+                                    verbatimTextOutput("action"),
+                                    verbatimTextOutput("errorput"),
+                                    verbatimTextOutput("locationin"),
+                                    column(12,leafletOutput("map"),
+                                           conditionalPanel('input.submit4 >0 && input.submit > 0',
+                                                            absolutePanel(id="legend",
+                                                                          class = "panel panel-default",
+                                                                          col = 'red',
+                                                                        
+                                                                         
+                                                                          top = 0, left = "auto", right = 0, bottom = "auto",
+                                                                          width = 100, height = 150,
+                                                                          
+                                                                          h5("Select Features"),
+                                                                          checkboxInput("Bus", label = "Bus",value= FALSE),
+                                                                          checkboxInput("Subway",label="Subway",value = FALSE))
+                                                            
+                                                            
+                                           )
+                                           
+                                           
+                                           ),
+                                    verbatimTextOutput("c1"),
+                                    verbatimTextOutput("c2"),
+                                    
+                                    verbatimTextOutput("target1"),
+                                    verbatimTextOutput("target3"),
+                                    verbatimTextOutput("target2")
+                                  )
+                                )
+                        
+                                  
+                                  
+                                  
+                              
+                  
+                                  
+                                  
+                                  
+                                )
+                        )
+                      )
+)
+
+
+
+                    
