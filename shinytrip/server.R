076a91bee3b21a23fdf19639b8dcb00d1e5b1bb7
Yuqiao <yl3965@users.noreply.github.com>
2019-02-27 17:05:28 -0500

    new version

---
+++ shinytrip/server.R
@@ -0,0 +1,467 @@
+library(geosphere)
+library(ggmap)
+library(maptools)
+library(leaflet)
+library(leaflet.extras)
+library(readr)
+library(shinyalert)
+restaurant<-data.frame(na.omit(read_csv("restaurant_final.csv")))
+type <- unique(as.character(restaurant$CUISINE))
+namedata<-c("Restaurant","Museum","Theatre","Gallery","Library")
+Gallery<-na.omit(read.csv("Gallery.csv",header=T))
+Library<-na.omit(read.csv("Library.csv",header=T))
+Museum<-na.omit(read.csv("Museum.csv",header=T))
+Theatre<-na.omit(read.csv("Theater.csv",header=T))
+all_data<-list(Gallery=Gallery,Library=Library,Museum=Museum,Restaurant=restaurant,Theatre=Theatre)
+register_google("AIzaSyA8OuCvy04PC3N-K9y6DdEc32hUpNyUrl8")
+load('subway_new.Rdata')
+load('bus.stop.Rdata')
+
+get_candidate<-function(data,Lon0,Lat0,r){
+  coords<-cbind(data$LON,data$LAT)
+  dis<-distm(coords,c(Lon0,Lat0), fun = distHaversine)
+  Ind<-dis<r
+  return(data[Ind,])
+}
+randomchoice<-function(data){
+  n<-nrow(data)
+  index<-sample(1:n,1)
+  return(data[index,])
+}
+# Define a server for the Shiny app
+#all_data1 = list(museum = museum,theatre = theatre, restaurant = restaurant)
+#namedata1 = c('museum','theatre','restaurant')
+Group <- c("Chinese","American","European","QuickMeal","Latin","Other","Asian",    
+           "Pizza","Coffee","Drink&Sweets")
+function(input, output) {
+  
+  
+  #################Clear Choices############
+  #################Clear Choices############
+  observeEvent(input$button2,{
+    proxy<-leafletProxy("map1")
+    proxy %>%
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) 
+    
+  })
+  
+  
+  
+  
+  
+  output$map1 <- renderLeaflet({
+    
+    map <- leaflet() %>% setView(-73.983,40.7639,zoom = 13)  %>% addProviderTiles(providers$Esri.WorldTopoMap)
+      
+    
+    for (i in 1:5){
+      n<-nrow(all_data[[namedata[i]]])
+      leafletProxy('map1',data=all_data[[namedata[i]]]) %>%
+        addMarkers(
+          clusterOptions = markerClusterOptions(),
+          lng=all_data[[namedata[i]]]$LON,
+          lat=all_data[[namedata[i]]]$LAT,
+          icon=list(iconUrl=paste('icon/',namedata[i],'.png',sep = ""),iconSize=c(20,20)),
+          popup=paste("Name:",all_data[[namedata[i]]]$NAME,"<br/>",
+                      "Tel:",all_data[[namedata[i]]]$TEL,"<br/>",
+                      "Zipcode:",all_data[[namedata[i]]]$ZIP,"<br/>",
+                      "Address:",all_data[[namedata[i]]]$ADDRESS),
+          group=namedata[i])
+    }
+    map%>%hideGroup(c("Restaurant","Museum","Theatre","Gallery","Library")) 
+    
+    Type<-as.character(unique(restaurant$CUISINE))
+ 
+    for (i in 1:length(Group)){
+      leafletProxy("map1") %>%
+        addMarkers(
+          data=restaurant[restaurant$CUISINE==Type[i],],
+          clusterOptions = markerClusterOptions(),
+          lng=restaurant[restaurant$CUISINE==Type[i],]$LON,lat=restaurant[restaurant$CUISINE==Type[i],]$LAT,
+          icon=list(iconUrl=paste('icon/','Restaurant','.png',sep = ""),iconSize=c(20,20)),
+          popup=paste("Name:",restaurant$NAME,"<br/>",
+                      "Tel:",restaurant$TEL,"<br/>",
+                      "Rating:",restaurant$RATING,"<br/>",
+                      "Price:",restaurant$PRICE,"<br/>",
+                      "Cuisine:",restaurant$CUISINE,"<br/>",
+                      "Tag:",restaurant$TAG,"<br/>",
+                      "Address:",restaurant$ADDRESS,"<br/>",
+                      "Grade:",restaurant$GRADE,"<br/>"
+                      ),
+          group=Group[i] )
+
+    }
+    map%>%hideGroup(Group)
+    
+  })
+  
+  observeEvent(input$region1,{
+    leafletProxy("map1") %>%hideGroup(c(namedata,Group))%>%
+      showGroup(input$region1)
+    
+    
+  })    
+  
+  
+  observeEvent(input$region2,{
+    if(input$region2 == "NA" ){leafletProxy("map1")%>%showGroup(input$region1)}
+    else {leafletProxy("map1")%>%
+        hideGroup(c(namedata,Group))%>%
+        showGroup(c(input$region2,input$region1))}
+  })
+  
+  
+  
+  observeEvent(input$region3,{
+    if("NA" == input$region3){leafletProxy("map1")%>%showGroup(c(input$region1,input$region2))}
+    else {leafletProxy("map1")%>%
+        hideGroup(c(namedata,Group))%>%
+        showGroup(c(input$region3,input$region2,input$region1))}
+  })
+  
+  observeEvent(input$type1,{
+    if(input$type1 == 'ALL'){leafletProxy("map1")%>%showGroup(c(input$region1,input$region2,input$region2))}
+    else {leafletProxy("map1")%>%
+        hideGroup(c(namedata))%>%
+        showGroup(c(input$type1))}
+  })
+  
+ # showStatus = reactive({
+#    if (is.null(input$map_bounds)){
+#      return ("1")
+#    } else {
+#      if (input$map_zoom<16){
+#        return ("2")
+#      } else {
+#        return ("3")
+#      }
+#    }
+#  })
+  
+  # get the museum data in the bounds
+  MuseumInBounds <- reactive({
+    if (is.null(input$map1_bounds))
+      return(Museum[FALSE,])
+    bounds <- input$map1_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    return(
+      subset(Museum,
+             LAT>= latRng[1] & LAT <= latRng[2] &
+               LON >= lngRng[1] & LON <= lngRng[2])
+    )
+  })
+  
+  # get the gallery data in the bounds
+  GalleryInBounds <- reactive({
+    if (is.null(input$map1_bounds))
+      return(Gallery[FALSE,])
+    bounds <- input$map1_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    return(
+      subset(Gallery,
+             LAT>= latRng[1] & LAT <= latRng[2] &
+               LON >= lngRng[1] & LON <= lngRng[2])
+    )
+  })
+  # get the library data in the bounds
+  LibraryInBounds <- reactive({
+    if (is.null(input$map1_bounds))
+      return(Library[FALSE,])
+    bounds <- input$map1_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    return(
+      subset(Library,
+             LAT>= latRng[1] & LAT <= latRng[2] &
+               LON >= lngRng[1] & LON <= lngRng[2])
+    )
+  })
+  # get the theatre data in the bounds
+  TheatreInBounds <- reactive({
+    if (is.null(input$map1_bounds))
+      return(Theatre[FALSE,])
+    bounds <- input$map1_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    return(
+      subset(Theatre,
+             LAT>= latRng[1] & LAT <= latRng[2] &
+               LON >= lngRng[1] & LON <= lngRng[2])
+    )
+  })
+  
+  # get the restaurant data in the bounds
+  RestaurantInBounds <- reactive({
+    if (is.null(input$map1_bounds))
+      return(restaurant[FALSE,])
+    bounds <- input$map1_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    return(
+      subset(restaurant,
+             LAT>= latRng[1] & LAT <= latRng[2] &
+               LON >= lngRng[1] & LON <= lngRng[2])
+    )
+  })
+  
+  #subset dataset
+  observe({
+    museum_subset = MuseumInBounds()
+    theatre_subset = TheatreInBounds()
+    restaurant_subset = RestaurantInBounds()
+    library_subset=LibraryInBounds()
+    gallery_subset=GalleryInBounds()
+    
+    if (nrow(museum_subset) != 0){
+      Museum = museum_subset
+    }
+    
+    if (nrow(theatre_subset) != 0){
+      Theatre = theatre_subset
+    }
+    
+    if (nrow(restaurant_subset) != 0){
+      restaurant = restaurant_subset
+    }
+    
+    if (nrow(library_subset) != 0){
+      Library = library_subset
+    }
+    
+    if (nrow(gallery_subset) != 0){
+      Gallery = gallery_subset
+    }
+    
+    output$table1 <- renderDataTable({
+      
+      if (input$region1 == 'Museum'){
+        print(Museum[,c(1,2,3)])
+        
+      }
+      else if(input$region1 == 'Theatre'){
+        print(Theatre[,c(1,2,4)])
+        
+      }
+      else if(input$region1 == 'Gallery'){
+        print(Gallery[,c(1,2,4)])
+      }
+      else if(input$region1 == 'Library'){
+        print(Library[,c(1,2,5)])
+      }
+      else if(input$region1 == 'Restaurant') {
+        if (input$type1 == 'ALL'){
+          print(restaurant[,c(1,5,9,6)])
+        }
+        else{
+          print(restaurant[restaurant$CUISINE == as.character(input$type1),c(1,5,9,6)])
+        }
+        
+      }
+    },
+    options = list(
+      scrollX=T,
+      pageLength = 5,
+      lengthMenu = c(3,5,8))) 
+    output$table2 <- renderDataTable({
+      
+      if (input$region2 == 'Museum'){
+        print(Museum[,c(1,2,3)])
+        
+      }
+      else if(input$region2 == 'Theatre'){
+        print(Theatre[,c(1,2,4)])
+        
+      }
+      else if(input$region2 == 'Gallery'){
+        print(Gallery[,c(1,2,4)])
+      }
+      else if(input$region2 == 'Library'){
+        print(Library[,c(1,2,5)])
+      }
+      else if(input$region2 == 'Restaurant') {
+        if (input$type1 == 'ALL'){
+          print(restaurant[,c(1,5,9,6)])
+        }
+        else{
+          print(restaurant[restaurant$CUISINE == as.character(input$type1),c(1,5,9,6)])
+        }    
+      }
+    },
+      options = list(
+        scrollX=T,
+        pageLength = 5,
+        lengthMenu = c(3,5,8)))
+    output$table3 <- renderDataTable({
+      
+      if (input$region3 == 'Museum'){
+        print(Museum[,c(1,2,3)])
+        
+      }
+      else if(input$region3 == 'Theatre'){
+        print(Theatre[,c(1,2,4)])
+        
+      }
+      else if(input$region3 == 'Gallery'){
+        print(Gallery[,c(1,2,4)])
+      }
+      else if(input$region3 == 'Library'){
+        print(Library[,c(1,2,5)])
+      }
+      else if(input$region3 == 'Restaurant') {
+        if (input$type1 == 'ALL'){
+          print(restaurant[,c(1,5,9,6)])
+        }
+        else{
+          print(restaurant[restaurant$CUISINE == as.character(input$type1),c(1,5,9,6)])
+        }
+      }
+    },options = list(
+      scrollX=T,
+      pageLength = 5,
+      lengthMenu = c(3,5,8))
+    )
+    }
+      )
+ 
+    
+    observeEvent(input$submit,{
+      if(input$location=="Current Location"){
+        coord<-getlatlng()
+        lat<-coord[1]
+        long<-coord[2]
+        output$map<-renderLeaflet(
+          {    map <- leaflet() %>% setView(-73.983,40.7639,zoom = 9) %>% addTiles()
+          map <- addControlGPS(map, options = gpsOptions(position = "topleft", activate = TRUE, 
+                                                         autoCenter = TRUE, maxZoom = 10, 
+                                                         setView = TRUE))
+          activateGPS(map)
+          
+          
+          })
+        output$action=renderPrint({coord})
+        
+      }else{
+        coord<-geocode(input$location)
+        output$action=renderPrint({coord})
+        lat<-as.numeric(coord[2])
+        long<-as.numeric(coord[1])
+        if(is.na(lat)&is.na(long)){ 
+          shinyalert("Please enter a valid Address!",type="error")
+        }else{
+          output$map<-renderLeaflet(
+            {    map<-leaflet() %>% addTiles%>%
+              addMarkers(lng=long,lat=lat)
+            
+            
+            })
+        }
+      }
+      
+      # random choice
+      
+      
+      output$locationin<-renderText(input$location)
+      observeEvent(input$submit4,{
+        index<-sample(1:4,2,replace=F)
+        choice<-c("Theatre","Museum","Gallery","Library","Restaurant")
+        choice1<-choice[index[1]]
+        choice2<-choice[index[2]]
+        index[3]<-length(choice)
+        choice3<-choice[index[3]]
+        
+        
+        data_candidate<-all_data[c(choice1,choice2,choice3)]
+        
+        ##Get candidates according to the radius:
+        data_select<-lapply(data_candidate,get_candidate,Lon0=long,Lat0=lat,r=input$distance*1000)
+        
+        targetplan<-lapply(data_select,randomchoice)
+        target1 <- as.character(targetplan[[1]][1,"NAME"])
+        target2 <- as.character(targetplan[[2]][1,"NAME"])
+        target3 <- as.character(targetplan[[3]][1,"NAME"])
+        
+        output$msg1 = renderText({
+          paste("Interested in ", {choice1}, " and ", {choice2}, "? ", sep="")
+        })
+        
+        output$msg2 = renderText({
+          paste("Why not spend your morning in ", {target1}, ", enjoy the lunch at ",
+                {target3}, ", and finish your day at ", {target2}, "?", sep="")
+        })
+        
+        output$msg3 = renderText({
+          paste("Click the icons on the map for more information. Not satisfied? Click
+                'Feeling Lucky' again for another recommendation!", sep="")
+        })
+        
+        
+        leafletProxy("map")%>%
+          clearMarkerClusters()
+        
+        for (i in 1:2){
+          leafletProxy("map",data=targetplan[[i]]) %>%
+            addMarkers(clusterOptions = markerClusterOptions(),
+                       lng=targetplan[[i]]$LON,lat=targetplan[[i]]$LAT,
+                       icon=list(iconUrl=paste('icon/',choice[index[i]],'.png',sep = ""),iconSize=c(20,20)),
+                       popup=paste("Name:",a(targetplan[[i]]$NAME,href = targetplan[[i]]$URL),"<br/>",
+                                   "Tel:",targetplan[[i]]$TEL,"<br/>",
+                                   "Address:",targetplan[[i]]$ADDRESS),
+                       group=choice[index[i]],layerId = i )
+        }
+        i<-3
+        leafletProxy("map",data=targetplan[[i]]) %>%
+          addMarkers(clusterOptions = markerClusterOptions(),
+                     lng=targetplan[[i]]$LON,lat=targetplan[[i]]$LAT,
+                     icon=list(iconUrl=paste('icon/',choice[index[i]],'.png',sep = ""),iconSize=c(20,20)),
+                     popup=paste(img(src=targetplan[[i]]$IMAGE_URL,width=80,height=80),"<br/>",
+                       "Name:",a(targetplan[[i]]$NAME,href = targetplan[[i]]$URL),"<br/>",
+                                 "Tel:",restaurant$TEL,"<br/>",
+                                 "Rating:",restaurant$RATING,"<br/>",
+                                 "Price:",restaurant$PRICE,"<br/>",
+                                 "Cuisine:",restaurant$CUISINE,"<br/>",
+                                 "Tag:",restaurant$TAG,"<br/>",
+                                 "Address:",restaurant$ADDRESS,"<br/>",
+                                 "Grade:",restaurant$GRADE,"<br/>"),
+                     group=choice[index[i]],layerId = i )
+        observeEvent(input$Subway,{
+          p<-input$Subway
+          proxy<-leafletProxy("map")
+          
+          if(p==TRUE){
+            proxy %>% 
+              addMarkers(data=subway, ~lng, ~lat,label = ~info,icon=icons(
+                iconUrl = "icon/icons8-Bus-48.png",
+                iconWidth = 10, iconHeight = 10),group="subway")
+          }
+          else proxy%>%clearGroup(group="subway")
+          
+        })
+        
+        ###############bus###############
+        observeEvent(input$Bus,{
+          p<-input$Bus
+          proxy<-leafletProxy("map")
+          
+          if(p==TRUE){
+            proxy %>% 
+              addMarkers(data=bus.stop, ~lng, ~lat,label = ~info,icon=icons(
+                iconUrl = "icon/bus.png",
+                iconWidth =10, iconHeight = 10),layerId=as.character(bus.stop$info))
+          }
+          else proxy%>%removeMarker(layerId=as.character(bus.stop$info))
+          
+        })
+        
+        
+      })
+      
+    }) 
+    
+  }
---
+++ shinytrip/ui.r
@@ -0,0 +1,213 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(ggplot2)
+library(shinyjs)
+library(shinydashboard)
+library(shiny)
+library(shinyBS)
+library(shinyWidgets)
+library(leaflet.extras)
+library(rebird)
+library(geosphere)
+library(ggmap)
+library(shinyalert)
+# Use a fluid Bootstrap layout
+
+restaurant<- read.csv("restaurant_final.csv")
+type <- unique(as.character(restaurant$CUISINE))
+
+ui <- dashboardPage(
+  skin = "yellow",
+                    dashboardHeader(title= "Day Planner"),
+                    dashboardSidebar(
+                      sidebarMenu(
+                        id="tabs",
+                        menuItem("Welcome",tabName = "Page_1",icon = icon("home")),
+                        menuItem("Search", tabName = "Page_2",icon = icon("search-plus")),
+                        menuItem("Feeling Lucky", tabName = "Page_3",icon = icon("laugh-wink"))
+                      )
+                    ),
+                    dashboardBody(
+                      tags$head(
+                        tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+                      ),
+                      tabItems(
+                        
+                        #Page 1 tab content
+                        
+                        tabItem(tabName = "Page_1",
+                                
+                              
+                           
+                                div(class = "Page_1",
+                                
+                                tags$head(
+                                  # Include our custom CSS
+                                  includeCSS("www/page1.css"),
+                                  includeScript("www/click_hover.js")
+                                  
+                                ),
+                                
+                                align="center",
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                br(),
+                                h1("Your Travel Suggestions in Manhattan",style="color:white;font-family: Times New Roman;font-size: 300%;font-weight: bold;"),
+                                br(),
+                                br(),
+                                h3("Gourp 8 - Spring 2019",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;"),
+                                h5("Caihui Xiao",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;"),
+                                h5("Charlie Chen",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;"),
+                                h5("Weixuan Wu",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;"),
+                                h5("Xiaoxi Zhao",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;"),
+                                h5("Yuqiao Li",style="color:white;font-family: Times New Roman;font-size: 200%;font-weight: bold;")
+                                
+                                
+                                
+                                
+                                
+                        )
+                                
+                        ),
+                        
+                        # First tab content
+                        tabItem(tabName = "Page_2",
+                                ################# First Row #################
+                                fluidRow(
+                                  column(width = 1,
+                                         style = "width:150px;display:inline-block;margin-right: 20px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                         selectInput("region1", "Choice 1:", c("Restaurant","Museum","Theatre","Gallery","Library"))
+                                  ),
+                                  column(width = 1,
+                                         style = "width:150px;display:inline-block;margin-right: 0px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                         selectInput("region2", "Choice 2:", c("Restaurant","Museum","Theatre","Gallery","Library","NA"),"NA")
+                                  ),
+                                  column(width = 1,
+                                         style = "width:150px;display:inline-block;margin-right: 0px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                         selectInput("region3", "Choice 3:", c("Restaurant","Museum","Theatre","Gallery","Library","NA"),"NA")
+                                  ),
+                                  column(width = 1,
+                                         style = "width:150px;display:inline-block;margin-right: 0px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                         conditionalPanel(
+                                           "input.region1 == 'Restaurant' || input.region2 == 'Restaurant' || input.region3 == 'Restaurant'" ,
+                                           selectInput('type1','Restaurant Type',c("ALL",type))
+                                         )
+                                  ),
+                                  column(width=1, 
+                                         style = "margin-top:25px;display:inline-block;margin-left:500px;",
+                                         actionButton("button2",label="Reset Map") 
+                                  )
+                                ),
+                                
+                                ################ Second Row ###################
+                                mainPanel(
+                                  fluidRow(
+                                    column(7,
+                                           #style = "width:500px;display:inline-block;margin-right:0px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                           tabsetPanel(type = "tabs",
+                                                       tabPanel("Choice 1", dataTableOutput("table1")),
+                                                       tabPanel("Choice 2", dataTableOutput("table2")),
+                                                       tabPanel("Choice 3", dataTableOutput("table3"))
+                                           )
+                                    ),
+                                    
+                                    column(width = 5,
+                                           #style = "width:300px;display:inline-block;margin-right:0px;margin-bottom:0px;margin-top:0px;padding-right:0px",
+                                           leafletOutput("map1", width = "250%", height = 550)
+                                          
+                                           
+                                    )
+                                  )
+                                )
+                        ),
+                        
+                        # Second tab content
+                        tabItem(tabName = "Page_3",
+                                tags$head(
+                                  tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+                                ),
+                                
+                                div(id="pg3_header",
+                                    #titlePanel("No idea at all?"),
+                                    h3(HTML('&nbsp;'),HTML('&nbsp;'),HTML('&nbsp;'),"No idea at all?"),
+                                    h4(HTML('&nbsp;'),HTML('&nbsp;'),HTML('&nbsp;'),"Just tell us where you are and how far you want to travel. We'll make a perfect plan for your day!")
+                                ),
+                              
+                                
+                                mainPanel(
+                                  useShinyalert(),
+                                  column(6,
+                                         fluidRow(
+                                           column(8,
+                                          
+                                           textInput("location",placeholder = "Enter Your Location",label = NULL, value="Current Location",width = "400px")),
+                                           column(4,
+                                                  
+                                                  div(id="confirm", actionButton("submit","Confirm",icon("map-marker"), width  ="100px")))
+                                         ),
+                                         
+                                         br(),p(),
+                                         sliderInput("distance", "Please choose distance from you (in km):", min = 0, max = 20, value = 1, step= 0.1, width = "500px"),
+                                         br(),
+                                         actionButton("submit4", label="Feeling Lucky!",style="opacity:0.9",align="left"),
+                                         br(),p(),br(),p(),
+                                         
+                                         div(id = "random_choice", 
+                                             h4(textOutput("msg1")),br(),
+                                             h4(textOutput("msg2")),br(),
+                                             h4(textOutput("msg3"))
+                                             )
+                                         
+                                         ),
+                                  column(6,
+                                         leafletOutput("map",width = "200%", height = 550),
+                                         conditionalPanel('input.submit4 >0 && input.submit > 0',
+                                                          absolutePanel(id="legend",
+                                                                        class = "panel panel-default",
+                                                                        
+                                                                        
+                                                                        top = 0, left = "auto", right = -278, bottom = "auto",
+                                                                        width = 100, height = 150,
+                                                                        
+                                                                        h5("Select Features"),
+                                                                        checkboxInput("Bus", label = "Bus",value= FALSE),
+                                                                        checkboxInput("Subway",label="Subway",value = FALSE)
+                                         )
+                                )
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                              
+                                           
+                                           
+                                           )
+                                    
+                                  )
+                                )
+                                  
+                                  
+                                  
+                                )
+                        )
+                      )
+
+
+
+
+                    
